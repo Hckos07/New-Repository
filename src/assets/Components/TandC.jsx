a47231cba20ffb42653bb08587614f9f3891070e
Abhay Pal <abhaypal1298@gmail.com>
2024-12-17 16:50:49 +0530

    third commit

--- src/assets/Components/TandC.jsx
+++ src/assets/Components/TandC.jsx
@@ -11,7 +11,7 @@ const TermsAndConditions = () => {
           <img
             src={terms}
             alt="Document Icon"
-            className="w-full h-auto object-cover rounded-lg shadow-2xl"
+            className="min-full h-auto object-cover rounded-lg shadow-2xl"
           />
         </div>
 
@@ -19,48 +19,54 @@ const TermsAndConditions = () => {
         <div className="max-w-xl text-center md:text-left ml-24 mt-24">
           <h1 className="text-3xl md:text-4xl font-bold text-blue-700 mb-4 md:mb-6">Terms and Conditions</h1>
           <p className="text-gray-700 text-base md:text-lg">
-            These Software house world Software Terms of Use Agreement ("Terms of Use") are entered into by
-            and between Software House Private Limited, a company registered under laws of India
-            ("Tally") and You ("Customer"), for Products and Services, being sold by software House.
+            Important – Please Read Carefully Before Downloading, Installing, or Using This Software.
+            By downloading or installing this software from Software House, you confirm that you have read this license agreement, understand its terms, and agree to be bound by them. If you are entering into this agreement on behalf of a company or other legal entity, you represent and warrant that you have the authority to bind such entity and its affiliates to these terms and conditions. In such cases, the terms “you” or “your” shall refer to the entity and its affiliates.
+            If you do not have the authority to bind your organization, or if you do not agree to these terms and conditions, you must not download, install, or use this software.
           </p>
         </div>
       </div>
 
       {/* Definitions Section */}
       <div className="mx-auto w-full pt-12">
-        <h2 className="text-3xl font-bold text-blue-700 mb-6">Definitions</h2>
+        <h2 className="text-3xl font-bold text-blue-700 mb-6">General Limitations</h2>
         <ul className="text-gray-700 text-lg space-y-4 list-disc pl-6">
-          <li><strong>Company:</strong> Software house world Private Limited, a company incorporated in the Republic of India.</li>
-          <li><strong>Customer, User, You, Your:</strong> Shall mean any individual, entity, or organization, having entered into any commercial transaction with Tally and whose data is being collected.</li>
-          <li><strong>Tally Partners or Suppliers:</strong> Shall mean businesses who are authorized to sell Tally Products and Services by Tally.</li>
-          <li><strong>Personal Data, Data:</strong> Refers to all the personal non-anonymized data provided by the Customer.</li>
-          <li><strong>Software:</strong> Each product available consists of an ACTIVATION KEY and one or more COMPONENTS (executables, documentation files, help files, language support dictionaries, data conversion and management utilities, edition management utilities, installers, scripts and/or other binaries/files).</li>
-          <li><strong>Extension:</strong> Extending and/or enhancing and/or modifying the working of the software is called extension. Extensions are allowed exclusively through the use of APIs (Application Programming Interface) published by the company for the purpose of extending, enhancing, or modifying the interface and/or data handling capability and/or operational processes. The terms customization, extension, add-on, module, and other similar terms may be used to denote the extensions made to the interface and/or data handling capability of the software.</li>
-          <li><strong>Documentation:</strong> It means user manuals, training materials, Product descriptions and specifications, technical manuals, license agreements, supporting materials and other information relating to Products or Services offered by TALLY, whether distributed in print, electronic, CD-ROM, or video format.</li>
-          <li><strong>Price List:</strong> It means the price list(s) published at tallysolutions.com applicable to the relevant TALLY entity to which each Purchase Order is issued by Customer. Products on the Price List are categorized, and discounts provided by TALLY will vary by category.</li>
-          <li><strong>Product:</strong> It means, individually or collectively as appropriate, Software and Documentation listed on the then-current Price List.</li>
+          <strong>Except as otherwise expressly provided under this License Agreement, You shall have no right, and You specifically agree not to:</strong>
+          <li>Utilize the Software beyond the applicable term;</li>
+          <li>Transfer, assign or sublicense Your license rights to any other person, and any such attempted transfer, assignment or sublicense shall be void;</li>
+          <li>Provide, divulge, disclose, or make available to, or permit the use of the Software by any third party;</li>
+          <li>Sell, resell, license, sublicense, distribute, rent or lease the Software or include the Software in a service bureau or outsourcing offering;</li>
+          <li>Make error corrections to or otherwise modify or adapt the Software or create derivative works based upon the Software, or to permit third parties to do the same;</li>
+          <li>Decompile, decrypt, reverse engineer, disassemble or otherwise reduce the Software to human-readable form, or to permit third parties to do the same;</li>
+          <li>Circumvent or disable any features or technological protection measures in the Software;</li>
         </ul>
       </div>
 
       {/* Scope Section */}
       <div className="mx-auto w-full pt-12">
-        <h2 className="text-3xl font-bold text-blue-700 mb-6">Scope</h2>
+        <h2 className="text-3xl font-bold text-blue-700 mb-6">Warranty and Limitation of Liability</h2>
         <p className="text-gray-700 text-lg mb-4">
-          These Terms of Use set forth the terms and conditions for Customer’s Purchase / Subscription and use of Products and Services.
+          The software is provided “as is” without warranty of any kind. All express, implied or statutory conditions, representations, and warranties including, without limitation, any implied warranty or condition of merchantability, fitness for a particular purpose, non-infringement, satisfactory quality or arising from a course of dealing, law, usage, or trade practice, are hereby excluded to the maximum extent allowed by applicable law. Neither Software House nor its licensors shall be liable for your action, or failure to act, in reliance on any information furnished as part of the software. You are solely responsible for maintaining the security of your network and computer systems. Neither Software House nor its licensors represent, warrant, or guarantee that (a) security threats, malicious code and/or vulnerabilities will be identified, or (b) the content will render your network and systems safe from malicious code, vulnerabilities, intrusions, or other security breaches, (c) every vulnerability on every tested system or application will be discovered, or (d) there will be no false positives.</p>
+        <p className="text-gray-700 text-lg mb-4">
+          In no event will Software House or its licensors be liable to you or your employees, or any third party, for any lost revenue, profit, or data, business interruption, or for special, indirect, consequential, incidental, or punitive damages, however caused and regardless of the theory of liability arising out of the use of or inability to use the software even if Software House have been advised of the possibility of such damages. In no event shall Software House be liable to you, whether in contract, warranty or tort (including negligence or strict liability), or otherwise, exceed the fee paid by you.
         </p>
       </div>
 
       {/* Prohibited Actions Section */}
       <div className="mx-auto w-full pt-12">
-        <h2 className="text-3xl font-bold text-blue-700 mb-6">Prohibited Actions</h2>
+        <h2 className="text-3xl font-bold text-blue-700 mb-6">Terms and Termination</h2>
         <p className="text-gray-700 text-lg mb-4">
-          Tally’s products and services may be used according to these terms, and Customer agrees not to:
+          This License Agreement is effective until terminated or the end of the applicable term. You may terminate this License Agreement at any time (i) by destroying all copies of Software, related documentation, analysis data and report and purging same from memory devices (required at the end of the applicable term). Your rights under this License Agreement will terminate immediately without notice from Software House if You fail to comply with any provision of this Agreement. Upon any termination, You must destroy all copies of Software and related documentation and purge same from memory devices. All provisions of this License Agreement relating to disclaimers of warranties, limitation of liabilities, remedies, damages protection of information and shall survive termination.
         </p>
-        <ul className="text-gray-700 text-lg space-y-4 list-disc pl-6">
-          <li>Use the Software for any purpose other than Customer’s internal business use.</li>
-          <li>Resell or make available to any third party, without a validly issued reseller certification or distribution rights granted by Tally.</li>
-          <li>Modify, reverse engineer, or decompile the Software except as permitted by law.</li>
-        </ul>
+      </div>
+        <div className="mx-auto w-full pt-12">
+          <h2 className="text-3xl font-bold text-blue-700 mb-6">General</h2>
+          <p className="text-gray-700 text-lg mb-4">
+          This License Agreement shall be governed by and construed in accordance with the laws of INDIA.
+          </p>
+        <h1><u>Cancellation & Refund Policy</u></h1>
+        <p className="text-gray-700 text-lg ">
+          Products/Services once ordered cannot be cancelled except when the purchase/subscription made, does not meet customer needs and specifications. You shall then initiate the return process. However, Software House may at its sole discretion consider accepting return of a product/service and grant of part or full refund, depending on the circumstances of each case.
+        </p>
       </div>
     </section>
   );
